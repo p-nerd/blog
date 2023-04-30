ee71ed79e8d5d328748b855c58dfe673289f4d6e
Shihab Mahamud <shihab4t@gmail.com>
2023-05-01 00:27:13 +0600

    Change site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,7 +28,7 @@ export default defineConfig(
                 },
             ],
         },
-        site: "http://localhost:3000/",
+        site: "https://p-nerd.github.io/blog",
         integrations: [sitemap()],
     }
 );
